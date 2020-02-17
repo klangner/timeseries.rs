a7ecfea8a03c0f9d3e835d54ee45ca6e7f4024fe
wenta <jakubwenta@gmail.com>
2020-02-17 21:54:23 +0100

    Create map function. Fixed #1

--- src/lib.rs
+++ src/lib.rs
@@ -98,6 +98,46 @@ impl TimeSeries {
         println!("{} -> {}", timestamp, pos);
         if pos > 0 { self.nth(pos-1) } else { 0.0 }
     }
+
+    /// Map over values
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use timeseries::TimeSeries;
+    ///
+    /// let index = vec![100, 160, 220];
+    /// let data = vec![1.0, 2.5, 3.2];
+    /// fn double(z: f32) -> f32{2.0 * z}
+    /// let ts = TimeSeries::new(index, data);
+    /// assert_eq!(ts.map_values(double).data, vec![2.0, 5.0, 6.4])
+    /// ```
+    #[inline]
+    pub fn map_values(&self, f: fn(f32) -> f32) -> TimeSeries {
+        let data2: Vec<f32> = self.data.iter().map(|&x| f(x)).collect();
+        return TimeSeries::new(self.index.to_vec(), data2);
+    }
+
+    /// Map over indexes and  values
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use timeseries::TimeSeries;
+    ///
+    /// fn double_even_index(i: i64, d: f32) -> f32 { if i & 1 == 0 { 2.0 * d } else { d }}
+    /// let data = vec![1.0, 2.5, 3.2, 4.0, 3.0];
+    /// let index = (0..data.len()).map(|i| i as i64).collect();        
+    /// let ts = TimeSeries::new(index, data);
+    /// assert_eq!(ts.map(double_even_index).data, vec![2.0, 2.5, 6.4, 4.0, 6.0]);
+    /// ```
+    #[inline]
+    pub fn map(&self, f: fn(i64, f32) -> f32) -> TimeSeries {
+        let zipped = self.index.iter().zip(self.data.iter());
+        let data2: Vec<f32> = zipped.map(|(&x, &y)| f(x,y)).collect();
+        //let data2: Vec<f32> = self.data.iter().map(|&x| f(x)).collect();
+        return TimeSeries::new(self.index.to_vec(), data2);
+    }
 }
 
 
@@ -107,13 +147,32 @@ impl TimeSeries {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn test_create() {
         let data = vec![1.0, 2.5, 3.2, 4.0, 3.0];
         let index = (0..data.len()).map(|i| 60*i as i64).collect();        
         let ts = TimeSeries::new(index, data);
-
         assert_eq!(ts.length(), 5);
+    }
+
+    #[test]
+    fn test_map() { 
+        fn double_even_index(i: i64, d: f32) -> f32 { if i & 1 == 0 { 2.0 * d } else { d }}
+        let data = vec![1.0, 2.5, 3.2, 4.0, 3.0];
+        let data2 = vec![2.0, 2.5, 6.4, 4.0, 6.0];
+        let index = (0..data.len()).map(|i| i as i64).collect();        
+        let ts = TimeSeries::new(index, data);
+        assert_eq!(ts.map(double_even_index).data, data2);
+    }
+
+    #[test]
+    fn test_map_values() {
+        fn double(z: f32) -> f32{2.0 * z}
+        let data = vec![1.0, 2.5, 3.2, 4.0, 3.0];
+        let data2 = vec![2.0, 5.0, 6.4, 8.0, 6.0];
+        let index = (0..data.len()).map(|i| 60*i as i64).collect();        
+        let ts = TimeSeries::new(index, data);
+        assert_eq!(ts.map_values(double).data, data2);
     }
 }
